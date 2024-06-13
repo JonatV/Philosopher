8ec9cbbadd31f4ba2222eeb7996d81a5b751674a
JonatV <mrangelsjon@gmail.com>
2024-06-13 19:52:24 +0200

    feat: join the pthread before it ends

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: jveirman <jveirman@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 16:49:47 by jveirman          #+#    #+#             */
-/*   Updated: 2024/06/13 19:02:02 by jveirman         ###   ########.fr       */
+/*   Updated: 2024/06/13 19:51:58 by jveirman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,19 @@ int	main(int c, char *v[])
 	t_prog			program;
 	t_philo			philos[MAX_PHILO];
 	t_mutex			m_forks[MAX_PHILO];
+	int				i;
 
 	config = parse_party(c, v);
 	if (config.error)
 		return (EXIT_FAILURE);
 	init(&program, &config, m_forks, philos);
 	create(&program, m_forks);
+	i = 0;
+	while (i < program.num_philo)
+	{
+		pthread_join(philos[i].thread, NULL);
+		i++;
+	}
 	end(&program, m_forks);
 	return (0);
 }
